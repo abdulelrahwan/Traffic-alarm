6607830a43a2d2239233b61ed60b6ba5b8992826
Abdul <abdulelrahwan@gmail.com>
2018-01-28 11:44:33 -0500

    final update

--- app/src/main/java/com/example/abdulelrahwa/myapplicationfr/MainActivity.java
+++ app/src/main/java/com/example/abdulelrahwa/myapplicationfr/MainActivity.java
@@ -1,86 +1,98 @@
 package com.example.abdulelrahwa.myapplicationfr;
 
-import android.os.AsyncTask;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    //int valueint = 0;
 
     private TextView tvData;
+    EditText home;
+    EditText destination;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btnHit = (Button)findViewById(R.id.btnHit);
-        tvData = (TextView)findViewById(R.id.tvJsonItem);
+        final MediaPlayer music21 = MediaPlayer.create(this, R.raw.music);
 
-        btnHit.setOnClickListener(new View.OnClickListener(){
-        public void onClick(View w) {
-            String origins = " f";
-            new JSONTask().execute("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&key=AIzaSyC7ZCMYbXM7VEDeJFXsy57MEB7buQxOhso&origins=1128%20dundas%20street%20west%20mississauga%20on&destinations=100+city+center+drive+missississauga+on");
-        }
-        });
+        Button btnHit = (Button) findViewById(R.id.btnHit);
 
-    }
+        tvData = (TextView) findViewById(R.id.tvJsonItem);
+        final MainActivity mainclass = this;
+
+        btnHit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View w) {
+                // player = MediaPlayer.create(getApplicationContext(), R.raw.music);
 
 
-    public class JSONTask extends AsyncTask<String, String, String> {
-        @Override
-        protected String doInBackground(String... params) {
-            HttpURLConnection connection = null;
-            BufferedReader reader = null;
+                // music21.start();
 
-            try {
-                URL url = new URL(params[0]);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.connect();
 
-                InputStream stream = connection.getInputStream();
+                EditText home = (EditText) findViewById(R.id.homebox);
+                EditText destination = (EditText) findViewById(R.id.editText2);
 
-                reader = new BufferedReader(new InputStreamReader(stream));
+                String origins = home.getText().toString();
+                origins = origins.replace(" ", "%20");
+                String destinationvar = destination.getText().toString();
+                destinationvar = destinationvar.replace(" ", "%20");
 
-                StringBuffer buffer = new StringBuffer();
 
-                String line = "";
-                while ((line = reader.readLine()) != null) {
-                    buffer.append(line);
-                }
-                return buffer.toString();
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (connection != null) {
-                    connection.disconnect();
-                }
-                try {
-                    if (reader != null) {
-                        reader.close();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                String URLOriginal = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&key=AIzaSyC7ZCMYbXM7VEDeJFXsy57MEB7buQxOhso&origins=" + origins + "&destinations=" + destinationvar;
+
+                JSONTask task = new JSONTask(mainclass);
+
+                task.execute(URLOriginal);
+
 
             }
-            return null;
-        }
-        protected void onPostExecute(String result){
-            super.onPostExecute(result);
-            tvData.setText(result);
-        }
+        });
+    }
+
+    public void setAlarm(int time) {
+        final MediaPlayer music21 = MediaPlayer.create(this, R.raw.music);
+
+        CountDownTimer timer = new CountDownTimer(time * 1000, 1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+            }
+
+            @Override
+            public void onFinish() {
+                // TODO Auto-generated method stub
+
+                music21.start();
+            }
+        };
+        timer.start();
+
+        EditText currentTime = (EditText) findViewById(R.id.editText6);
+        EditText readyTime = (EditText) findViewById(R.id.editText3);
+        EditText arrivalTime = (EditText) findViewById(R.id.editText4);
+
+
+        int rTime = Integer.parseInt(readyTime.getText().toString());
+        int aTime = Integer.parseInt(arrivalTime.getText().toString());
+
+        Intent i = new Intent(MainActivity.this, Alarm.class);
+        PendingIntent pi = PendingIntent.getBroadcast(getApplicationContext(), 0, i, 0);
+        AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
+        //final MediaPlayer music21 = MediaPlayer.create(this, R.raw.music);
+
+        am.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + aTime * 1000, pi);
+
     }
 
 }
